85c48142d6344b2c21378facf45aadb05c9fa037
zafar-m3n <thezafar.m3n@gmail.com>
2024-08-24 11:53:14 +0530

    Admin home

--- app/src/main/java/com/example/exploresrilanka/AdminHomeFragment.kt
+++ app/src/main/java/com/example/exploresrilanka/AdminHomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AdminHomeFragment : Fragment() {
 
@@ -12,7 +14,29 @@ class AdminHomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_admin_home, container, false)
+
+        val viewHotelListButton: Button = view.findViewById(R.id.view_hotel_list_button)
+        val viewDestinationsListButton: Button = view.findViewById(R.id.view_destinations_list_button)
+
+        viewHotelListButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.admin_fragment_container, AdminBookingsFragment())
+                .addToBackStack(null)
+                .commit()
+
+            (activity as? AdminActivity)?.findViewById<BottomNavigationView>(R.id.admin_bottom_navigation)?.selectedItemId = R.id.nav_bookings
+        }
+
+        viewDestinationsListButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.admin_fragment_container, AdminExploreFragment())
+                .addToBackStack(null)
+                .commit()
+
+            (activity as? AdminActivity)?.findViewById<BottomNavigationView>(R.id.admin_bottom_navigation)?.selectedItemId = R.id.nav_explore
+        }
+
+        return view
     }
 }
